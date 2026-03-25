d023a5257f6a5aa2f8e8d494a6cb3f9b8d5cb34e
{varun} <{abc@gmail.com}>
2026-03-25 22:17:37 +0530

    Improved model performance by role normalization, removing non-tech roles, and filtering rare classes; Macro F1 increased significantly

--- V3/Dataset_preprocessing.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import seaborn as sns
-df1 = pd.read_csv('D:/Git_hub/p1/V3/Dataset_Kaggle/dataset1.csv')
-print(df1.shape)
-df2 = pd.read_csv('D:/Git_hub/p1/V3/Dataset_Kaggle/dataset2.csv')
-print(df2.shape)
-df3 = pd.read_csv('D:/Git_hub/p1/V3/Dataset_Kaggle/dataset3.csv')
-print(df3.shape)
